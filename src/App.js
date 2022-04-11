bead9118080237d76ee81dabb943dc84c8b3d7d1
Lera Abramova <67120537+Vareria-s@users.noreply.github.com>
2022-04-11 13:16:34 +0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import img from "./img/polygon2.png";
+import search from "./img/search.png"
+import vector from "./img/vector.png"
+import ws from "./img/ws.png"
+import telegram from "./img/telegram.png"
+import shopping from "./img/shopping-bag.png"
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="app-wrapper">
+            <div className="Header">
+                <div className="first-menu">
+                    <div className="languages">
+                        <div>ru</div>
+                        <img src={img} alt=""/>
+                        <ul className="сhange-language">
+                            <li>ru</li>
+                            <li>en</li>
+                        </ul>
+                    </div>
+
+                    <div className="first-menu-block">
+                        <div className="item">Новости и блог</div>
+                        <div className="item">Оплата и доставка</div>
+                        <div className="item">Контакты</div>
+                        <div className="item">Вакансии</div>
+                    </div>
+                    <div className="search">
+                        <input placeholder="Поиск...">
+                        </input>
+                        <img src={search} alt="">
+                        </img>
+                    </div>
+                </div>
+
+
+                <div className="second-menu">
+                    <div className="second-menu-block">
+                        <div className="item">Каталог
+                        </div>
+                        <div className="item">
+                            Майнинг отель
+                            <img src={vector} alt=""/>
+                            <ul className="dropdown-menu">
+                                <li>Калининская АЭС</li>
+                                <li>ПАО “Тулачермет”</li>
+                            </ul>
+                        </div>
+                        <div className="item">
+                            Лизинг
+                        </div>
+                        <div className="item">
+                            О компании
+                        </div>
+                    </div>
+                    <div className="contact-information">
+                        <div className="telephone">+7 (499) 877-41-45</div>
+                        <div className="contact-free">
+                           <div className="social-media">
+                               <img src={telegram} alt=""/>
+                               <img src={ws} alt=""/>
+                           </div>
+                            <div className="back-call">Заказать звонок</div>
+                        </div>
+                    </div>
+                    <div className="basket">
+                        <div className="basket-picture">
+                            <img src={shopping} alt=""/>
+                            <div className="amount"><p>2</p></div>
+                        </div>
+                        <div className="basket-text">Корзина</div>
+                    </div>
+                </div>
+            </div>
+            <div className="content"></div>
+        </div>
+      </BrowserRouter>
   );
 }
 
